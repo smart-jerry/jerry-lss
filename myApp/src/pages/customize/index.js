dd00eee19603cc360aee7b6f060c6b77fe08ed3f
pengqin <jerry@jollycorp.com>
2019-04-12 11:29:58 +0800

    定制

--- myApp/src/pages/customize/index.js
+++ myApp/src/pages/customize/index.js
@@ -2,12 +2,165 @@
  * Created by jerry on 2019/4/2.
  */
 
-import Taro, {Component} from '@tarojs/taro'
+import Taro, {Component} from '@tarojs/taro';
+import {View, Form, Text, Input, RadioGroup, Label, Radio, Button} from '@tarojs/components';
+import './index.less';
 
 class Index extends Component{
+  config = {
+    navigationBarTitleText: '定制'
+  }
+  
+  constructor (props){
+    super(props);
+    this.state={
+      // 定制类型
+      typeList:[{
+        'value':'0',
+        'checked':true,
+        'text':'戒指'
+      },{
+          'value':'1',
+          'checked':false,
+          'text':'吊坠'
+        },{
+        'value':'2',
+        'checked':false,
+        'text':'摆件'
+      },{
+        'value':'3',
+        'checked':false,
+        'text':'耳钉'
+      }],
+      // 戒指封口类型
+      rangList:[{
+        'value':'0',
+        'checked':true,
+        'text':'活口'
+      },{
+      'value':'1',
+        'checked':false,
+        'text':'闭口'
+    }],
+      // 镶嵌材质
+      QualityList:[
+        {
+          'value':'0',
+          'checked':true,
+          'text':'玫瑰金'
+        },{
+          'value':'1',
+          'checked':false,
+          'text':'白银'
+        }
+      ],
+      //宝贝类型选择
+      selectType:0,
+      // 戒指封口类型选择
+      selectRangType:0
+    }
+  }
+  //宝贝类型选择
+  selectTypeEvt(e){
+    this.setState({
+      selectType:e.detail.value
+    })
+  }
+  //戒指封口类型选择
+  selectRangTypeEvt(e){
+    this.setState({
+      selectRangType:e.detail.value
+    })
+  }
   render(){
     return(
-      <div> this is customization page.</div>
+      <View className="custom-box">
+        <Form>
+          <View className='form-group'>
+            <Text>宝贝类型：</Text>
+            <RadioGroup onChange={this.selectTypeEvt} name="typeSelect">
+              {this.state.typeList.map((item, i) => {
+                return (
+                  <Label className='radio-list__label' for={i} key={i}>
+                    <Radio className='radio-list__radio' value={item.value} checked={item.checked}>{item.text}</Radio>
+                  </Label>
+                )
+              })}
+            </RadioGroup>
+          </View>
+          <View>aaa:{this.state.selectType}---{this.state.selectType === 0},,,,,bbb:{this.state.selectRangType}---{this.state.selectRangType === 1}</View>
+          {/*戒指镶嵌材质*/}
+          {
+            this.state.selectType === 0 &&
+              <View className="form-group"  name="bbb">
+                <Text>戒指镶嵌材质：</Text>
+                <RadioGroup>
+                  {this.state.QualityList.map((item, i) => {
+                    return (
+                      <Label className='radio-list__label' for={i} key={i}>
+                        <Radio className='radio-list__radio' value={item.value} checked={item.checked}>{item.text}</Radio>
+                      </Label>
+                    )
+                  })}
+                </RadioGroup>
+              </View>
+            
+          }
+          {/*戒指封口类型*/}
+          {
+            this.state.selectType === 0 &&
+              <View className="form-group">
+                <Text>戒指封口类型：</Text>
+                <RadioGroup onChange={this.selectRangTypeEvt} name="aaa">
+                  {this.state.rangList.map((item, i) => {
+                    return (
+                      <Label className='radio-list__label' for={i} key={i}>
+                        <Radio className='radio-list__radio' value={item.value} checked={item.checked}>{item.text}</Radio>
+                      </Label>
+                    )
+                  })}
+                </RadioGroup>
+              </View>
+            
+          }
+          {/*手指尺寸*/}
+          {
+            this.state.selectRangType === 1 &&
+              <View className="form-group">
+                <Text>手指尺寸(单位:mm)：</Text>
+                <Input type='digit' placeholder='请输入手指尺寸'/>
+              </View>
+            
+          }
+          
+          {/*耳钉镶嵌品质*/}
+          {
+            this.state.selectType === 3 &&
+              <View className="form-group" name="ddd">
+                <Text>耳钉镶嵌材质：</Text>
+                <RadioGroup>
+                  {this.state.QualityList.map((item, i) => {
+                    return (
+                      <Label className='radio-list__label' for={i} key={i}>
+                        <Radio className='radio-list__radio' value={item.value} checked={item.checked}>{item.text}</Radio>
+                      </Label>
+                    )
+                  })}
+                </RadioGroup>
+              </View>
+            
+          }
+          
+          <View className="form-group">
+            <Text>宝贝图样：</Text>
+            <Input type='text' placeholder='上传图片样式'/>
+          </View>
+          <View className="form-btn">
+            <Button className='btn-max-w mr30' form-type="reset" size="default" plain type='default'>重置</Button>
+            <Button className='btn-max-w submit-btn' form-type="submit" size="default" plain type='default'>提交</Button>
+          </View>
+        </Form>
+      </View>
     )
   }
 }
